99eef16420b691e8c72dd54007e35a7e8e385f9b
Sofiane Amarouche <azroubar@gmail.com>
2018-02-27 17:26:04 -0500

    added search page code

---
+++ src/main/java/com/qa/pages/SearchPage.java
@@ -0,0 +1,12 @@
+package com.qa.pages;
+
+public class SearchPage {
+	public void searchPage() {
+		System.out.println("searchPage");
+	}
+	
+	public void loginSet() {
+		System.out.println("login set");
+	}
+
+}
